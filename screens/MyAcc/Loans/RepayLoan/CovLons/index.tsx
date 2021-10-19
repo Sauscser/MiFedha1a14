f9cb34e5f11e6befeb8e8b57a5cbf3d7ab6c9a4d
MiFedha_MyFedha <sauscser@gmail.com>
2021-10-19 08:49:52 -0700

    message12

---
+++ screens/MyAcc/Loans/RepayLoan/CovLons/index.tsx
@@ -0,0 +1,426 @@
+import React, {useEffect, useState} from 'react';
+
+import {
+  
+  createSmLoansCovered,
+  
+  createSmLoansNonCovered,
+  
+  createSmNonLoans,
+  
+  updateCompany,
+  
+  updateSmAccount,
+  
+} from '../../../../../src/graphql/mutations';
+
+import {API, Auth, graphqlOperation} from 'aws-amplify';
+import {
+  
+  getCompany,
+  getSmAccount,
+  
+} from '../../../../../src/graphql/queries';
+
+import {useNavigation} from '@react-navigation/native';
+
+import {
+  View,
+  Text,
+  ImageBackground,
+  Pressable,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Alert,
+  ActivityIndicator
+} from 'react-native';
+import styles from './styles';
+
+
+const RepayCovLnsss = props => {
+  const [SenderNatId, setSenderNatId] = useState('');
+  const [RecNatId, setRecNatId] = useState('');
+  const [SnderPW, setSnderPW] = useState("");
+  
+  const [amounts, setAmount] = useState("");
+  
+  const [Desc, setDesc] = useState("");
+  const [ownr, setownr] = useState(null);
+  const[isLoading, setIsLoading] = useState(false);
+  
+  
+
+  const fetchUser = async () => {
+    const userInfo = await Auth.currentAuthenticatedUser();
+    setownr(userInfo.attributes.sub);  
+     
+  }
+
+  useEffect(() => {
+    fetchUser();
+    }, []);  
+
+
+  const fetchSenderUsrDtls = async () => {
+    if(isLoading){
+      return;
+    }
+    setIsLoading(false);
+    try {
+      const accountDtl:any = await API.graphql(
+        graphqlOperation(getSmAccount, {nationalid: SenderNatId}),
+      );
+
+      const SenderUsrBal =accountDtl.data.getSMAccount.balance;
+      const usrPW =accountDtl.data.getSMAccount.pw;
+      const usrAcActvStts =accountDtl.data.getSMAccount.acStatus;
+      const SenderSub =accountDtl.data.getSMAccount.owner;
+      const ttlNonLonsSentSMs =accountDtl.data.getSMAccount.ttlNonLonsSentSM;
+      const loanLimits =accountDtl.data.getSMAccount.loanLimit;
+      
+      const fetchCompDtls = async () => {
+        if(isLoading){
+          return;
+        }
+        setIsLoading(true);
+        try {
+          const CompDtls:any = await API.graphql(
+            graphqlOperation(getCompany, {
+              AdminId: "BaruchHabaB'ShemAdonai2",
+            }),
+          );
+          
+            
+          const UsrTransferFee = CompDtls.data.getCompany.userTransferFee;
+          const TotalTransacted = parseFloat(amounts)  + parseFloat(UsrTransferFee)*parseFloat(amounts);
+          const CompPhoneContact = CompDtls.data.getCompany.phoneContact;      
+          
+          const companyEarningBals = CompDtls.data.getCompany.companyEarningBal;
+          const companyEarnings = CompDtls.data.getCompany.companyEarning;
+          const ttlNonLonssRecSMs = CompDtls.data.getCompany.ttlNonLonssRecSM;
+          const ttlNonLonssSentSMs = CompDtls.data.getCompany.ttlNonLonssSentSM; 
+         
+                    
+          const fetchRecUsrDtls = async () => {
+            if(isLoading){
+              return;
+            }
+            setIsLoading(true);
+            try {
+                const RecAccountDtl:any = await API.graphql(
+                    graphqlOperation(getSmAccount, {nationalid: RecNatId}),
+                    );
+                    const RecUsrBal =RecAccountDtl.data.getSMAccount.balance;                    
+                    const usrAcActvSttss =RecAccountDtl.data.getSMAccount.acStatus; 
+                    const ttlNonLonsRecSMs =RecAccountDtl.data.getSMAccount.ttlNonLonsRecSM;
+                    
+                    
+                  
+                    const sendSMNonLn = async () => {
+                      if(isLoading){
+                        return;
+                      }
+                      setIsLoading(true)
+                      try {
+                        await API.graphql(
+                          graphqlOperation(createSmNonLoans, {
+                            input: {
+                              recId: RecNatId,
+                              senderID: SenderNatId,                                  
+                              amount: amounts,                              
+                              description: Desc,
+                              status: "LoanActive",
+                              owner: ownr
+                            },
+                          }),
+                        );
+
+
+                      } catch (error) {
+                        if(!error){
+                          Alert.alert("Account deactivated successfully")
+                          
+                      } 
+                      else{Alert.alert("Please check your internet connection")
+                      return;}
+                      }
+                      setIsLoading(false);
+                      await updtSendrAc();
+                    };
+
+                    const updtSendrAc = async () =>{
+                      if(isLoading){
+                        return;
+                      }
+                      setIsLoading(true);
+                      try{
+                          await API.graphql(
+                            graphqlOperation(updateSmAccount, {
+                              input:{
+                                nationalid:SenderNatId,
+                                ttlNonLonsSentSM: parseFloat(ttlNonLonsSentSMs)+parseFloat(amounts),
+                                balance:parseFloat(SenderUsrBal)-TotalTransacted 
+                               
+                                
+                              }
+                            })
+                          )
+
+
+                      }
+                      catch(error){
+                        if (error){Alert.alert("Check your internet connection")
+                        return;}
+                      }
+                      setIsLoading(false);
+                      await updtRecAc();
+                    }
+
+                    const updtRecAc = async () =>{
+                      if(isLoading){
+                        return;
+                      }
+                      setIsLoading(true);
+                      try{
+                          await API.graphql(
+                            graphqlOperation(updateSmAccount, {
+                              input:{
+                                nationalid:RecNatId,
+                                ttlNonLonsRecSM: parseFloat(ttlNonLonsRecSMs) + parseFloat(amounts) ,
+                                balance:parseFloat(RecUsrBal) + parseFloat(amounts)                                     
+                                
+                                                                  
+                                
+                              }
+                            })
+                          )                              
+                      }
+                      catch(error){
+                        if (error){Alert.alert("Check your internet connection")
+                        return;}
+                      }
+                      setIsLoading(false);
+                      await updtComp();
+                    }
+
+                    const updtComp = async () =>{
+                      if(isLoading){
+                        return;
+                      }
+                      setIsLoading(true);
+                      try{
+                          await API.graphql(
+                            graphqlOperation(updateCompany, {
+                              input:{
+                                AdminId: "BaruchHabaB'ShemAdonai2",                                                      
+                               
+                                companyEarningBal:UsrTransferFee * parseFloat(amounts) + parseFloat(companyEarningBals),
+                                companyEarning: UsrTransferFee * parseFloat(amounts) + parseFloat(companyEarnings),                                                    
+                                
+                                ttlNonLonssRecSM: parseFloat(amounts) + parseFloat(ttlNonLonssRecSMs),
+                                ttlNonLonssSentSM: parseFloat(amounts) + parseFloat(ttlNonLonssSentSMs),
+                                
+                              }
+                            })
+                          )
+                          
+                          
+                      }
+                      catch(error){
+                        if (error){Alert.alert("Check your internet connection")
+                    return;}
+                      }
+                      setIsLoading(false);
+                    }
+                    
+                                          
+                    
+                    if(usrAcActvStts !== "AccountActive"){Alert.alert('Sender account is inactive');}
+                    else if(usrAcActvSttss !== "AccountActive"){Alert.alert('Receiver account is inactive');}
+                    
+                    else if (
+                      SenderUsrBal < TotalTransacted 
+                    ) {Alert.alert('Requested amount is more than you have in your account');}
+                    
+                    else if(usrPW !==SnderPW){Alert.alert('Wrong password');}
+                    else if(ownr !==SenderSub){Alert.alert('Please send from your own  account');}
+                    
+                    else if(loanLimits < amounts){Alert.alert('Call ' + CompPhoneContact + ' to have your send Amount limit adjusted');}
+                    
+                     else {
+                      sendSMNonLn();
+                    }                                                
+                }       
+                catch(e) {     
+                  if (e){Alert.alert("Reciever does not exist")
+  return;}                 
+                }
+                setIsLoading(false);
+                }                    
+                  await fetchRecUsrDtls();
+        } catch (e) {
+          if (e){Alert.alert("Check your internet connection")
+      return;}
+        }
+        setIsLoading(false);        
+      };
+      await fetchCompDtls();
+    
+      
+    } catch (e) {
+      if (e){Alert.alert("Sender does not exist")
+      return;}
+  };
+      setIsLoading(false);
+      setSenderNatId('');
+      setAmount("");
+      setRecNatId('');
+      
+      setDesc("");
+      setSnderPW("");
+      
+}
+
+useEffect(() =>{
+  const SnderNatIds=SenderNatId
+    if(!SnderNatIds && SnderNatIds!=="")
+    {
+      setSenderNatId("");
+      return;
+    }
+    setSenderNatId(SnderNatIds);
+    }, [SenderNatId]
+     );
+
+     useEffect(() =>{
+      const amt=amounts
+        if(!amt && amt!=="")
+        {
+          setAmount("");
+          return;
+        }
+        setAmount(amt);
+        }, [amounts]
+         );
+
+         useEffect(() =>{
+          const RecNatIds=RecNatId
+            if(!RecNatIds && RecNatIds!=="")
+            {
+              setRecNatId("");
+              return;
+            }
+            setRecNatId(RecNatIds);
+            }, [RecNatId]
+             );
+
+             
+
+                 
+
+                     useEffect(() =>{
+                      const descr=Desc
+                        if(!descr && descr!=="")
+                        {
+                          setDesc("");
+                          return;
+                        }
+                        setDesc(descr);
+                        }, [Desc]
+                         );
+
+                         useEffect(() =>{
+                          const SnderPWss=SnderPW
+                            if(!SnderPWss && SnderPWss!=="")
+                            {
+                              setSnderPW("");
+                              return;
+                            }
+                            setSnderPW(SnderPWss);
+                            }, [SnderPW]
+                             );
+
+                             
+
+                                 
+
+  return (
+    <View>
+      <View
+        
+        style={styles.image}>
+        <ScrollView>
+         
+          <View style={styles.amountTitleView}>
+            <Text style={styles.title}>Fill account Details Below</Text>
+          </View>
+
+          <View style={styles.sendAmtView}>
+            <TextInput
+              value={SenderNatId}
+              onChangeText={setSenderNatId}
+              style={styles.sendAmtInput}
+              editable={true}></TextInput>
+            <Text style={styles.sendAmtText}>Loanee National Id</Text>
+          </View>
+
+          <View style={styles.sendAmtView}>
+            <TextInput
+              value={RecNatId}
+              onChangeText={setRecNatId}
+              style={styles.sendAmtInput}
+              editable={true}></TextInput>
+            <Text style={styles.sendAmtText}>Loaner National Id</Text>
+          </View>
+
+          <View style={styles.sendAmtView}>
+            <TextInput
+              value={amounts}
+              onChangeText={setAmount}
+              style={styles.sendAmtInput}
+              editable={true}
+              ></TextInput>
+              
+            <Text style={styles.sendAmtText}>Amount Sent</Text>
+          </View>
+
+
+          <View style={styles.sendAmtView}>
+            <TextInput
+              value={SnderPW}
+              onChangeText={setSnderPW}
+              style={styles.sendAmtInput}
+              editable={true}></TextInput>
+            <Text style={styles.sendAmtText}>Sender PassWord</Text>
+          </View>
+
+          
+
+          <View style={styles.sendAmtView}>
+            <TextInput
+              multiline={true}
+              value={Desc}
+              onChangeText={setDesc}
+              style={styles.sendAmtInputDesc}
+              editable={true}></TextInput>
+            <Text style={styles.sendAmtText}>Description</Text>
+          </View>
+
+          <TouchableOpacity
+            onPress={fetchSenderUsrDtls}
+            style={styles.sendAmtButton}>
+            <Text style={styles.sendAmtButtonText}>Send</Text>
+            {isLoading && <ActivityIndicator size = "large" color = "blue"/>}
+          </TouchableOpacity>
+
+          
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
+
+export default RepayCovLnsss;
