6f757452d84b174c81d38524db22630b34e09b5a
MiFedha_MyFedha <sauscser@gmail.com>
2021-11-01 08:56:05 -0700

    commit 29

--- screens/MFAdmin/Withdraw/index.tsx
+++ screens/MFAdmin/Withdraw/index.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 
 import {
+  createBankAdmWithdrawals,
   createFloatAdd,
   createFloatReduction, 
 
@@ -11,7 +12,7 @@ import {
   
 } from '../../../src/graphql/mutations';
 import {API, graphqlOperation, Auth} from 'aws-amplify';
-import {getAgent, getCompany, getSAgent, getSMAccount} from '../../../src/graphql/queries';
+import {getAgent, getBankAdmin, getCompany, getSAgent, getSMAccount} from '../../../src/graphql/queries';
 import {
   View,
   Text,
@@ -24,11 +25,11 @@ import {
 } from 'react-native';
 import styles from './styles';
 
-const SMADepositForm = props => {
+const AdminWthdwl = props => {
   const [WthDrwrPhn, setWthDrwrPhn] = useState(null);
 
   const[UsrPWd, setUsrPWd] = useState("");
-  const [AgentPhn, setAgentPhn] = useState("");
+  const [BnkkAdmNatId, setBnkkAdmNatId] = useState("");
   const [amount, setAmount] = useState("");
   const[isLoading, setIsLoading] = useState(false);
   const [ownr, setownr] = useState(null);
@@ -58,81 +59,39 @@ const SMADepositForm = props => {
       );
 
       const usrBala = accountDtl.data.getSMAccount.balance;      
-      const TtlWthdrwnSMs = accountDtl.data.getSMAccount.TtlWthdrwnSM;
+      
       const usrStts = accountDtl.data.getSMAccount.acStatus; 
-      const withdrawalLimits = accountDtl.data.getSMAccount.withdrawalLimit;  
-      const pws = accountDtl.data.getSmAccount.pw;
+      
+     
       const owners = accountDtl.data.getSMAccount.owner;
       const names = accountDtl.data.getSMAccount.name;
          
       
-      const fetchAgtBal = async () => {
+      const fetchAdmnDtls = async () => {
         if(isLoading){
           return;
         }
         setIsLoading(true);
         try {
-          const AgentBal:any = await API.graphql(
-            graphqlOperation(getAgent, {phonecontact: AgentPhn}),
+          const AdmnDtl:any = await API.graphql(
+            graphqlOperation(getBankAdmin, {nationalid: BnkkAdmNatId}),
           );    
          
-          const TtlFltIns = AgentBal.data.getAgent.TtlFltIn;
-          const floatBals = AgentBal.data.getAgent.floatBal;
-          const AgAcAct = AgentBal.data.getAgent.status;
-          const sagentregnos = AgentBal.data.getAgent.sagentregno;
-          const namess = AgentBal.data.getAgent.name;
-
-          const gtCompDtls = async () =>{
-            if(isLoading){
-              return;
-            }
-            setIsLoading(true);
-            try{
-              const compDtls :any= await API.graphql(
-              graphqlOperation(getCompany,{AdminId:"BaruchHabaB'ShemAdonai2"})
-                );
-                  const ttlUserWthdrwls = compDtls.data.getCompany.ttlUserWthdrwl;
-                  const agentComs = compDtls.data.getCompany.agentCom;
-                  const sagentComs = compDtls.data.getCompany.sagentCom;
-                  const companyComs = compDtls.data.getCompany.companyCom;
-                  const UsrWithdrawalFee = parseFloat(agentComs)*parseFloat(amount)
-                                            parseFloat(sagentComs)*parseFloat(amount)
-                                            parseFloat(companyComs)*parseFloat(amount)
-                  const AgentCommission = parseFloat(agentComs)*parseFloat(amount)
-                                                
-                  const saCommission =    parseFloat(sagentComs)*parseFloat(amount)
-                  
-                  const TTlAmtTrnsctd = parseFloat(amount) + UsrWithdrawalFee
-                  const compCommission = parseFloat(companyComs)*parseFloat(amount)
-                  const companyEarningBals = compDtls.data.getCompany.companyEarningBal
-                  const companyEarnings = compDtls.data.getCompany.companyEarning
-                  const agentEarningBals = compDtls.data.getCompany.agentEarningBal
-                  const agentEarnings = compDtls.data.getCompany.agentEarning
-                  const saEarningBals = compDtls.data.getCompany.saEarningBal
-                  const saEarnings = compDtls.data.getCompany.saEarning
-                  const agentFloatIns = compDtls.data.getCompany.agentFloatIn                 
-                  
-                  const gtsaDtls = async () =>{
-                    if(isLoading){
-                      return;
-                    }
-                    setIsLoading(true);
-                    try{
-                      const compDtls :any= await API.graphql(
-                      graphqlOperation(getSAgent,{saPhoneContact:sagentregnos})
-                        );
-                          const TtlEarningss = compDtls.data.getCompany.TtlEarnings;
-                          const saBalances = compDtls.data.getCompany.saBalance;
+          const BankAdmBals = AdmnDtl.data.getBankAdmin.BankAdmBal;
+          const pws = AdmnDtl.data.getBankAdmin.pw;
+          const names = AdmnDtl.data.getBankAdmin.name;
+          const statussssss = AdmnDtl.data.getBankAdmin.status;
+          
+          
                           
-                          const CrtFltAdd = async () => {
+                          const CrtAdmnWthdrwls = async () => {
                             try {
                               await API.graphql(
-                                graphqlOperation(createFloatAdd, {
+                                graphqlOperation(createBankAdmWithdrawals, {
                                   input: {
                                   
-                                    withdrawerid: WthDrwrPhn,                    
-                                    agentPhonecontact: AgentPhn,
-                                    sagentId: sagentregnos,
+                                    bankAdmNatId: BnkkAdmNatId,                    
+                                    
                                     owner: ownr,
                                     amount: amount,
                                     status: 'AccountActive',
@@ -163,8 +122,8 @@ const SMADepositForm = props => {
                             input: {
                               phonecontact: WthDrwrPhn,
                   
-                              balance: parseFloat(usrBala) - TTlAmtTrnsctd ,
-                              TtlWthdrwnSM: parseFloat(TtlWthdrwnSMs) + parseFloat(amount),
+                              balance: parseFloat(usrBala) + parseFloat(amount) ,
+                              
                             },
                           }),
                         );
@@ -174,103 +133,15 @@ const SMADepositForm = props => {
                         if (error){Alert.alert("Check internet Connection")
                         return;}
                       }
+                      Alert.alert(names + ", You have transfer Ksh. "+ amount +" to your SM account")
                       setIsLoading(false);
-                      await onUpdtAgntBal();
+                      
                       }; 
         
-                      const onUpdtAgntBal = async () => {
-                        if(isLoading){
-                          return;
-                        }
-                        setIsLoading(true);
-                        try {
-                          await API.graphql(
-                            graphqlOperation(updateAgent, {
-                              input: {
-                                phonecontact: AgentPhn,
+                      
                     
-                               
-                                TtlFltIn: parseFloat(TtlFltIns) + parseFloat(amount),
-                                floatBal: parseFloat(floatBals) + parseFloat(amount),
-                              },
-                            }),
-                          );
-                        }
-        
-                        catch (error) {
-                          if (error){Alert.alert("Check internet Connection")
-                          return;}
-                        }
-                        setIsLoading(false);
-                        await onUpdtsaDtls();
-                        }; 
-        
-                        
-        
-                          const onUpdtsaDtls = async () => {
-                            if(isLoading){
-                              return;
-                            }
-                            setIsLoading(true);
-                            try {
-                              await API.graphql(
-                                graphqlOperation(updateSAgent, {
-                                  input: {
-                                    saPhoneContact: sagentregnos,
-                        
-                                   
-                                    TtlEarnings: parseFloat(TtlEarningss) + saCommission,
-                                    saBalance: parseFloat(saBalances) + saCommission,
-                                      
-                                  },
-                                }),
-                              );
-                            }
-            
-                            catch (error) {
-                              if (error){Alert.alert("Check internet Connection")
-                              return;}
-                            }
-                            await onUpdtCompDtls();
-                            setIsLoading(false);
-                            }; 
-
-                            const onUpdtCompDtls = async () => {
-                              if(isLoading){
-                                return;
-                              }
-                                setIsLoading(true);
-                              try {
-                                await API.graphql(
-                                  graphqlOperation(updateCompany, {
-                                    input: {
-                                      AdminId:"BaruchHabaB'ShemAdonai2",                    
-                                     
-                                      companyEarningBal: parseFloat(companyEarningBals) + compCommission,
-                                      companyEarning: parseFloat(companyEarnings) + compCommission,
-                                      agentEarningBal: parseFloat(agentEarningBals) + AgentCommission,
-                                      agentEarning: parseFloat(agentEarnings) + AgentCommission,
-                                      saEarningBal: parseFloat(saEarningBals) + saCommission,
-                                      saEarning: parseFloat(saEarnings) + saCommission,
-                                      ttlUserWthdrwl: parseFloat(ttlUserWthdrwls) + parseFloat(amount),
-                                      agentFloatIn: parseFloat(agentFloatIns) + parseFloat(amount),
-                                      
-                                    },
-                                  }),
-                                );
-                              }
-              
-                              catch (error) {
-                                if (error){Alert.alert("Check internet Connection")
-                                return;}
-                              }
-                              setIsLoading(false);
-                              Alert.alert(names + " has withdrawn Ksh. " + amount + " from " + namess + " MFNdogo");
-                              }; 
-                    
-                    
-                    if (TTlAmtTrnsctd > parseFloat(usrBala)) {
-                      Alert.alert("Insufficient user Balance")
+                    if (parseFloat(amount) > parseFloat(BankAdmBals)) {
+                      Alert.alert("Insufficient Admin Balance")
                       return;
                     } 
         
@@ -279,45 +150,29 @@ const SMADepositForm = props => {
                       return;
                     } 
 
+                    else if (statussssss!=="AccountActive") {
+                      Alert.alert("Admin Account is inactive")
+                      return;
+                    } 
+
                     else if (ownr!==owners) {
                       Alert.alert("You cannot withdraw from another account")
                       return;
                     }  
 
-                    else if(parseFloat(amount)>parseFloat(withdrawalLimits)) {
-                      Alert.alert('Withdrawal limit exceeded');
-                      return;
-                    }
-                    if (AgAcAct!=="AccountActive") {
-                      Alert.alert("MFNdogo Account has been deactivated")
-                      return;
-                    } 
+                    
+                   
                     
                     if (UsrPWd!==pws) {
-                      Alert.alert("User credentials are wrong; access denied")
+                      Alert.alert("Admin credentials are wrong; access denied")
                       return;
                     } 
         
-                    else{await CrtFltAdd()}   
+                    else{await CrtAdmnWthdrwls()}   
         
         
-                    } catch (error) {
-                  if (error){Alert.alert("Check your internet connection")
-                          return;}
-                    }
-                    setIsLoading(false);
-                    };    
-        
-                    await gtsaDtls();         
-            
-            } catch (error) {
-          if (error){Alert.alert("Check your internet connection")
-                  return;}
-            }
-           setIsLoading(false);
-            };    
-
-            await gtCompDtls();           
+                   
+                    
       
     }     
     catch (e) {
@@ -328,7 +183,7 @@ const SMADepositForm = props => {
   setIsLoading(false);
 };
 
-    await fetchAgtBal();
+    await fetchAdmnDtls();
     }
 
     catch (e) {
@@ -340,7 +195,7 @@ const SMADepositForm = props => {
     
     setAmount("");
     setUsrPWd("")
-    setAgentPhn("");    
+    setBnkkAdmNatId("");    
   }; 
 
   
@@ -368,14 +223,14 @@ const SMADepositForm = props => {
                );
 
                useEffect(() =>{
-                const agphn=AgentPhn
-                  if(!agphn && agphn!=="")
+                const BnkkAdmNatIds=BnkkAdmNatId
+                  if(!BnkkAdmNatIds && BnkkAdmNatIds!=="")
                   {
-                    setAgentPhn("");
+                    setBnkkAdmNatId("");
                     return;
                   }
-                  setAgentPhn(agphn);
-                  }, [AgentPhn]
+                  setBnkkAdmNatId(BnkkAdmNatIds);
+                  }, [BnkkAdmNatId]
                    );
 
   
@@ -395,8 +250,8 @@ const SMADepositForm = props => {
 
           <View style={styles.sendAmtView}>
             <TextInput
-              value={AgentPhn}
-              onChangeText={setAgentPhn}
+              value={BnkkAdmNatId}
+              onChangeText={setBnkkAdmNatId}
               style={styles.sendAmtInput}
               editable={true}></TextInput>
             <Text style={styles.sendAmtText}>Agent Phone</Text>
@@ -431,4 +286,4 @@ const SMADepositForm = props => {
   );
 };
 
-export default SMADepositForm;
+export default AdminWthdwl;
